02c44f928a479641082a190abf0353e8bc786f5a
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-04-14 19:11:19 +0200

    Moved CSS file and uninstalled CSS plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,10 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-  ],
 };
